34f469d27fc5eec13d55b003cc17ddf53fb388a1
py-in-the-sky <ryanmwise1@gmail.com>
2014-10-18 19:23:12 -0700

    configure Flask debugger to work with GAE dev server

--- app/main.py
+++ app/main.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def hello():
     return 'Hello, world!'
 
-# uncomment line below if you wish to run in debug mode in your dev environment
+# uncomment lines below if you wish to run in debug mode in your dev environment
+# from werkzeug.debug import DebuggedApplication
 # app.debug = True
+# app = DebuggedApplication(app, evalex=True)
+
+# @app.route('/test')  # for intentionally triggering the Flask debugger
+# def test():
+#     assert app.debug == False
+#     return 'not in debug mode'
